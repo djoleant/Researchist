653ba6680c90e632414e5d9ebbab804df394af57
Matija Speletic <matijasp@elfak.rs>
2022-12-23 00:02:37 +0100

    Bug fix i dodavanje forme za kreiranje rada

--- ResearchistApp/src/index.js
+++ ResearchistApp/src/index.js
@@ -4,29 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import {createStore, combineReducers} from 'redux';
-import {Provider, useDispatch} from 'react-redux';
-import authReducer from './reducers/Auth';
+import { createStore, combineReducers } from 'redux';
+import { Provider, useDispatch } from 'react-redux';
+// import authReducer from './reducers/Auth';
 import thunkMiddleware from "redux-thunk"
-import {applyMiddleware} from "redux";
+import { applyMiddleware } from "redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(
-  combineReducers({
-      auth: authReducer,
-  }),
-  applyMiddleware(thunkMiddleware)
-);
+// const store = createStore(
+//   combineReducers({
+//     //auth: authReducer,
+//   }),
+//   applyMiddleware(thunkMiddleware)
+// );
 
 root.render(
-  
-    <Provider store={store}>
+
+  //<Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    </Provider>
-  
+  //</Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
