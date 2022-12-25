40d55937566192c5fcab679d36ffae262450b4b0
Djordje Antic <djoleant@iCloud.com>
2022-12-25 14:05:34 +0100

    Search edit

--- ResearchistApp/src/Search.js
+++ ResearchistApp/src/Search.js
@@ -18,27 +18,28 @@ import { useNavigate } from "react-router-dom";
 import PaperCard from "./components/Categories/PaperCard";
 import { useParams } from "react-router-dom";
 
-
 export default function Search(props) {
   const [search, setSearch] = useState("");
-  const [CategoryData, setCategoryData] = useState([]);
-  
-  const { id } = useParams();
-  const getCategories = async () => {
+  const [searchData, setSearchData] = useState([]);
+
+  const { id, param } = useParams();
+  const getSearchData = async () => {
     const response = await fetch(
-      "http://localhost:5211/api/HomeController2/GetCategories",
+      "http://localhost:5211/api/Home/Search/" + param,
       {
         credentials: "include",
+        method: "GET",
       }
     );
 
     const fetchData = await response.json();
     console.log(fetchData);
-    setCategoryData(fetchData);
+    setSearchData(fetchData);
   };
 
   useEffect(() => {
-    getCategories();
+    getSearchData();
+    // setSearchData("");
   }, []);
 
   const navigate = useNavigate();
@@ -48,15 +49,51 @@ export default function Search(props) {
       <CssBaseline />
       <React.Fragment>
         <Paper sx={{ p: 3, mb: 4 }} variant="outlined">
-        <Typography variant='h6' align="left">UNDER CONSTRUCTION</Typography>
-          {/* {CategoryData.map((info, index) => (
-            <Grid item xs={12} md={6} lg={4} key={index}>
-              <PaperCard
-                title={info.name}
-                id={info.id}
-              />
-            </Grid>
-          ))} */}
+          <Typography variant="h6" align="left">
+            UNDER CONSTRUCTION
+          </Typography>
+          <Grid item xs={12} style={{ padding: "10px", maxWidth: 500 }}>
+            <TextField
+              onChange={(event) => {
+                setSearch(event.target.value);
+                console.log("Search", search);
+                getSearchData();
+                console.log(searchData);
+              }}
+              id="outlined-basic-email"
+              label="Expand your knowledge..."
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+
+          {searchData.people !== undefined || searchData.people != null
+            ? searchData.people.map((info, index) =>
+                info.people !== null ? (
+                  <Grid item xs={12} md={6} lg={4} key={index}>
+                    <PaperCard title={info.name} id={info.id} />
+                  </Grid>
+                ) : (
+                  <Grid item xs={12} md={6} lg={4} key={index}>
+                    <PaperCard title={info.papers.title} id={info.papers.id} />
+                  </Grid>
+                )
+              )
+            : console.log("searchData is undef or null")}
+
+          {searchData.papers !== undefined || searchData.papers != null
+            ? searchData.papers.map((info, index) =>
+                info.papers !== null ? (
+                  <Grid item xs={12} md={6} lg={4} key={index}>
+                    <PaperCard title={info.title} id={info.id} />
+                  </Grid>
+                ) : (
+                  <Grid item xs={12} md={6} lg={4} key={index}>
+                    <PaperCard title={info.papers.title} id={info.papers.id} />
+                  </Grid>
+                )
+              )
+            : console.log("searchData is undef or null")}
         </Paper>
       </React.Fragment>
     </Container>
